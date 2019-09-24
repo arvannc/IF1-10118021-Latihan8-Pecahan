4da1bb17af267036e188de8a661429cc68e49974
M Arvan Nurcahyana <55648161+arvannc@users.noreply.github.com>
2019-09-24 18:32:35 +0700

    Add files via upload

---
+++ src/if1/pkg10118021/latihan8/pecahan/IF110118021Latihan8Pecahan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118021.latihan8.pecahan;
+
+/**
+ *
+ * @author Arvan
+ * NAMA     : M Arvan Nurcahyana
+ * KELAS    : IF-1
+ * NIM      : 10118021
+ * Deskripsi Program : Pecahan
+ */
+public class IF110118021Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double: " + d);
+        
+    }
+    
+}
